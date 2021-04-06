e1bf064e071ab8e05faec78767458616bcaddec2
Muhamad Fikri Al-Baihaqi <fikrialbaihaqi@gmail.com>
2021-04-07 04:11:43 +0700

    Menambah activity DetailTransaksi

---
+++ app/src/main/java/com/example/financeku/DetailTransaksi.java
@@ -0,0 +1,14 @@
+package com.example.financeku;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DetailTransaksi extends AppCompatActivity {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_detail_transaksi);
+  }
+}
